5975969204bf12f42ac1ed6c89a2fe1701c13c42
rayenbbs <rayenbesbesbesbes@gmail.com>
2024-04-28 16:56:19 +0100

    Finished the Header component

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+import mealsImage from "../../assets/Food_Image.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>IjaKool</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table with food on it" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
